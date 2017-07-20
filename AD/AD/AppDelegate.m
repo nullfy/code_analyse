5d1ae5ca2e051be1b6e63fe5daf724f1bb1e242a
nullfy <nullfy>
2017-07-20 19:20:39 +0800

    readme  update
    readme update

--- AD/AD/AppDelegate.m
+++ AD/AD/AppDelegate.m
@@ -20,12 +20,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     /*
-     目前LaunchScreen是不支持自定义类的，如果通过storyBoard的方式来设置LaunchImage，可以再这里加一句很关键的代码
+     目前LaunchScreen是不支持自定义类的，如果通过LaunchScreen.storyBoard的方式来设置LaunchImage，可以再这里加一句很关键的代码
      [self.window makeKeyAndVisible]; //在此默默感谢Desgard 大神的项目DGAdLaunchView提供了这个思路
      
-     为什么说这句代码很关键呢，如果你的App结构是tab-nav,或者 nav，那么通过xib的方式是无法正确显示广告页的
-     之前看到一个思路是在RootVC对应的那个VC里面的DidAppear里面会存在页面闪烁的情况。
+     为什么说这句代码很关键呢，如果你的App结构是tab-nav,或者 nav，那么通过xib的方式是无法正确显示广告页的，因为你直接在didFinishLaunchingWithOptions是无法获取到keywindow 的。
+     之前看到一个解决思路是在RootVC对应的那个VC里面的DidAppear里面加载启动广告，但是这会存在页面闪烁的情况。如果提前到ViewWillAppear又会出现广告页被遮挡的问题。这个问题可以在RockerHX大神的 LaunchScreenAnimation-Storyboard这个项目中了解更多的信息。
      
+     上面添加的这个代码作用也很明朗，提前将window创建好并可见，就可以避免xib加载时在didFinishLaunchingWithOptions 中无法获取windown的问题
+     顺便提一下，DGAdLaunchView 几乎可以满足常见的业务需求，至于网易新闻那样的广告页，我猜想应该是用自定义VC加转场动画做的，而不单单是一层轻量的View了吧。
      */
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
