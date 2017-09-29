d8502b1b45c67b3749c03d16cba381992de7e4bb
nullfy <nullfy>
2017-09-29 19:22:39 +0800

    Merge_Sorted_Array
    Merge_Sorted_Array

--- algorithms/algorithms/Easy_088_Merge_Sorted_Array.swift
+++ algorithms/algorithms/Easy_088_Merge_Sorted_Array.swift
@@ -22,14 +22,29 @@ import Cocoa
  
  Inspired by @leetchunhui at https://leetcode.com/discuss/8233/this-is-my-ac-code-may-help-you
  
+ 题解：给定两个已排序的数组，合并为一个，同时排好序
+ 1、从后往前填充
  */
 class Easy_088_Merge_Sorted_Array: NSObject {
-    static func merge(nums: inout [Int], m: Int, num2: [Int], n: Int) {
+    static func merge(num1: inout [Int], m: Int, num2: [Int], n: Int) {
         var i = m - 1
         var j = n - 1
         var k = m + n - 1
         while i >= 0 && j >= 0 {
-            
+            if num1[i] > num2[j] {
+                num1[k] = num1[i]
+                k -= 1
+                i -= 1
+            } else {
+                num1[k] = num2[j]
+                k -= 1
+                j -= 1
+            }
+        }
+        while j >= 0 {
+            num1[k] = num2[j]
+            k -= 1
+            j -= 1
         }
     }
 }
