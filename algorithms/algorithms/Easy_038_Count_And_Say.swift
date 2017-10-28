c19180d66eadf071bbdf95f002988ccf5e991ede
nullfy <nullfy>
2017-10-28 09:58:47 +0800

    Easy_038_Count_And_Say
    Easy_038_Count_And_Say

--- algorithms/algorithms/Easy_038_Count_And_Say.swift
+++ algorithms/algorithms/Easy_038_Count_And_Say.swift
@@ -26,7 +26,13 @@ import Cocoa
  Note: The sequence of integers will be represented as a string.
  
  Inspired by @zerobased at https://leetcode.com/discuss/7678/show-an-answer-in-java
- 题解：
+ 题解：这个题目有些难懂，简单的翻译一下就是，通过读一个prev字符串来生成一个新的字符串。
+ 最开始的字符串是”1“，读出来是"一个1"，写做11
+ 字符串“11“读出来是"两个1"，写做21
+ 字符串”21“读出来是”一个2一个1“，写做1211，以此类推
+ 字符串‘1211’读出来是“一个1一个2两个1”，写作111221
+ 字符串‘111221’读出来是“三个1两个2一个1”，写作312211
+ 1、
  */
 
 private extension String {
@@ -37,7 +43,7 @@ private extension String {
 class Easy_038_Count_And_Say: NSObject {
     class func conutAndSay (_ n: Int) -> String {
         var result: String = "1"
-        for _ in 0..<n {
+        for _ in 1..<n {
             let previous: String = result
             result = ""
             var counter = 1
@@ -52,6 +58,7 @@ class Easy_038_Count_And_Say: NSObject {
                 }
             }
             result = "\(result)\(counter)\(say)"
+            print("r\(result) c\(counter), s\(say)")
         }
         return result
     }
