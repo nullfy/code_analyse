0eae5da237a4a7a247e0b6f7085bcfe51b325f5d
nullfy <nullfy>
2017-12-07 22:24:22 +0800

    Search for a Range
    Search for a Range

---
+++ algorithms/algorithms/Medium/Medium_034_Search_For_A_Range.swift
@@ -0,0 +1,62 @@
+//
+//  Medium_034_Search_For_A_Range.swift
+//  algorithms
+//
+//  Created by 李晓东 on 2017/12/7.
+//  Copyright © 2017年 XD. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ https://leetcode.com/problems/search-for-a-range/
+ 
+ #34 Search for a Range
+ 
+ Level: medium
+ 
+ Given a sorted array of integers, find the starting and ending position of a given target value.
+ 
+ Your algorithm's runtime complexity must be in the order of O(log n).
+ 
+ If the target is not found in the array, return [-1, -1].
+ 
+ For example,
+ Given [5, 7, 7, 8, 8, 10] and target value 8,
+ return [3, 4].
+ 
+ Inspired by @stellari at https://leetcode.com/discuss/18242/clean-iterative-solution-binary-searches-with-explanation
+ 
+ */
+struct Medium_034_Search_A_Range {
+    static func searchRange(nums: [Int], target: Int) -> [Int] {
+        var i: Int = 0
+        var j: Int = nums.count - 1
+        var result: [Int] = Array.init(repeating: -1, count: 2)
+        while i < j {
+            let mid: Int = (i + j)/2
+            if nums[mid] < target {
+                i = mid + 1
+            } else {
+                j = mid
+            }
+        }
+        if nums[i] != target {
+            return result
+        } else {
+            result[0] = i
+        }
+        j = nums.count - 1
+        while i < j {
+            let mid: Int = (i + j)/2 + 1
+            if nums[mid] > target {
+                j = mid - 1
+            } else {
+                i = mid
+            }
+        }
+        result[1] = j
+        return result
+    }
+}
